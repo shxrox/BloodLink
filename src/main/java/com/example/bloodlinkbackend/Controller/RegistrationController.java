ba1a9474c22ff1d29bf4959d01522d00a22e0cb4
shxrox <sharondeva8@gmail.com>
2025-05-02 19:32:35 +0530

    backend

--- src/main/java/com/example/bloodlinkbackend/Controller/RegistrationController.java
+++ src/main/java/com/example/bloodlinkbackend/Controller/RegistrationController.java
@@ -1,10 +1,29 @@
 package com.example.bloodlinkbackend.Controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/users")
+import com.example.bloodlinkbackend.Model.Registration;
+import com.example.bloodlinkbackend.Service.RegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/register")
+@CrossOrigin(origins = "*")
 public class RegistrationController {
 
+    @Autowired
+    private RegistrationService service;
+
+    @PostMapping
+    public ResponseEntity<?> registerUser(@Valid @RequestBody Registration user) {
+        if (service.emailExists(user.getEmail())) {
+            return ResponseEntity.badRequest().body("Email already exists");
+        }
+
+        service.register(user);
+        return ResponseEntity.ok("Registration successful");
+    }
 }
--- src/main/java/com/example/bloodlinkbackend/Service/RegistrationService.java
+++ src/main/java/com/example/bloodlinkbackend/Service/RegistrationService.java
@@ -1,8 +1,22 @@
 package com.example.bloodlinkbackend.Service;
 
+import com.example.bloodlinkbackend.Model.Registration;
+import com.example.bloodlinkbackend.Repository.RegistrationRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegistrationService {
 
+    @Autowired
+    private RegistrationRepository repository;
+
+    public boolean emailExists(String email) {
+        return repository.findByEmail(email).isPresent();
+    }
+
+    public Registration register(Registration registration) {
+        return repository.save(registration);
+    }
 }
