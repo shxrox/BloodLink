ea258bb3ad4354b9bff52c720baf69e8735cccad
shxrox <sharondeva8@gmail.com>
2025-05-02 19:33:35 +0530

    frontend

--- BloodLine-Frontend/src/main.jsx
+++ BloodLine-Frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";  // Router
+import App from "./App.jsx";
+import "./index.css";  // Importing the CSS file
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>  {/* Wrapping the App with Router */}
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
