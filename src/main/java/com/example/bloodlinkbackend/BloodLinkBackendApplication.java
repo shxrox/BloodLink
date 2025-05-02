2d21f4839800e555a7ae4e60fb59943d476702e8
shxrox <sharondeva8@gmail.com>
2025-05-02 16:15:11 +0530

    Initial push

---
+++ src/main/java/com/example/bloodlinkbackend/BloodLinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bloodlinkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodLinkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloodLinkBackendApplication.class, args);
+    }
+
+}
