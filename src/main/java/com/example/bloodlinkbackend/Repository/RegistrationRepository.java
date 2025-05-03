5fb46994f0119d1dcbaec32a75136749add7028e
shxrox <sharondeva8@gmail.com>
2025-05-03 19:37:48 +0530

    fix reg repo

--- src/main/java/com/example/bloodlinkbackend/Repository/RegistrationRepository.java
+++ src/main/java/com/example/bloodlinkbackend/Repository/RegistrationRepository.java
@@ -2,9 +2,8 @@ package com.example.bloodlinkbackend.Repository;
 
 import com.example.bloodlinkbackend.Model.Registration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
+@Repository
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
-    Optional<Registration> findByEmail(String email);
 }
