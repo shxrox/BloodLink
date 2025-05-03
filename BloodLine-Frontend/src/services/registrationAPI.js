2f9f9307fa9ceaa4d907f638d0f2777623c86e8f
shxrox <sharondeva8@gmail.com>
2025-05-03 19:38:47 +0530

    RegisterAPI Code

---
+++ BloodLine-Frontend/src/services/registrationAPI.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const url = "http://localhost:8080/registration";
+
+export const getRegistrations = async () => {
+    const response = await axios.get(url);
+    return response.data;
+};
+
+export const getRegistrationById = async (id) => {
+    const response = await axios.get(`${url}/${id}`);
+    return response.data;
+};
+
+export const addRegistration = async (registration) => {
+    const response = await axios.post(url, registration);
+    return response.data;
+};
+
+export const updateRegistration = async (id, registration) => {
+    const response = await axios.put(`${url}/${id}`, registration);
+    return response.data;
+};
+
+export const deleteRegistration = async (id) => {
+    const response = await axios.delete(`${url}/${id}`);
+    return response.data;
+};
