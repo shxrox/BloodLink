9774ee280cafb7d239bf68bc54026ff66590ae43
shxrox <sharondeva8@gmail.com>
2025-05-03 19:36:53 +0530

    fix reg controller

--- src/main/java/com/example/bloodlinkbackend/Controller/RegistrationController.java
+++ src/main/java/com/example/bloodlinkbackend/Controller/RegistrationController.java
@@ -1,29 +1,42 @@
 package com.example.bloodlinkbackend.Controller;
 
-
 import com.example.bloodlinkbackend.Model.Registration;
 import com.example.bloodlinkbackend.Service.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/register")
-@CrossOrigin(origins = "*")
+@RequestMapping("/registration")
+@CrossOrigin(origins = "http://localhost:5173/")
 public class RegistrationController {
 
     @Autowired
-    private RegistrationService service;
+    private RegistrationService registrationService;
+
+    @GetMapping
+    public List<Registration> getAllRegistrations() {
+        return registrationService.getAllRegistrations();
+    }
+
+    @GetMapping("/{id}")
+    public Registration getRegistrationById(@PathVariable Long id) {
+        return registrationService.getRegistrationById(id);
+    }
 
     @PostMapping
-    public ResponseEntity<?> registerUser(@Valid @RequestBody Registration user) {
-        if (service.emailExists(user.getEmail())) {
-            return ResponseEntity.badRequest().body("Email already exists");
-        }
+    public Registration addRegistration(@RequestBody Registration registration) {
+        return registrationService.saveRegistration(registration);
+    }
 
-        service.register(user);
-        return ResponseEntity.ok("Registration successful");
+    @PutMapping("/{id}")
+    public Registration updateRegistration(@PathVariable Long id, @RequestBody Registration registration) {
+        return registrationService.updateRegistration(id, registration);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteRegistration(@PathVariable Long id) {
+        registrationService.deleteRegistration(id);
     }
 }
