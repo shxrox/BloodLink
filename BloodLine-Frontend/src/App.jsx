b17c10266ecb3cf6805c060952070767929f6801
shxrox <sharondeva8@gmail.com>
2025-05-04 20:37:46 +0530

    Navigation Update

--- BloodLine-Frontend/src/App.jsx
+++ BloodLine-Frontend/src/App.jsx
@@ -4,7 +4,7 @@ import RegistrationForm from './component/RegistrationForm';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<RegistrationForm />} />
+      <Route path="/registration" element={<RegistrationForm />} />
     </Routes>
   );
 }
