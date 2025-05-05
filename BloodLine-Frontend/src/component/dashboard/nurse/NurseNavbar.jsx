f33b5debfe2f9bc93b6acb192d2e4b67af9a60d3
shxrox <sharondeva8@gmail.com>
2025-05-05 17:18:19 +0530

    nurse navbar

---
+++ BloodLine-Frontend/src/component/dashboard/nurse/NurseNavbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NurseNavbar = () => {
+  return (
+    <nav style={styles.navbar}>
+      <div>
+        <Link to="/dashboard/nurse" style={styles.link}>Home</Link>
+        <Link to="/dashboard/nurse/patient-register" style={styles.link}>patient Register</Link>
+        <Link to="/login" style={styles.link}>Logout</Link>
+      </div>
+    </nav>
+  );
+};
+
+const styles = {
+  navbar: {
+    backgroundColor: "green",
+    padding: "10px",
+    display: "flex",
+    justifyContent: "center",
+  },
+  link: {
+    margin: "0 15px",
+    color: "white",
+    textDecoration: "none",
+    fontWeight: "bold",
+  }
+};
+
+export default NurseNavbar;
