49fc77d522722c7a7607bcc3fed12b05efc3b613
shxrox <sharondeva8@gmail.com>
2025-05-05 17:17:59 +0530

    nurse dashboard

--- BloodLine-Frontend/src/component/dashboard/nurse/nurse.jsx
+++ BloodLine-Frontend/src/component/dashboard/nurse/nurse.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "../nurse/NurseNavbar"; 
 
 const Nurse = () => {
   return (
     <div>
+      <Navbar />
       <h1>Welcome Nurse</h1>
       <p>You have successfully logged in.</p>
     </div>
