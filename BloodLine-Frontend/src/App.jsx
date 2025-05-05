13c7fac2b5526140cf193748b1ed675df69ce464
shxrox <sharondeva8@gmail.com>
2025-05-05 13:39:41 +0530

    adding routing

--- BloodLine-Frontend/src/App.jsx
+++ BloodLine-Frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import RegistrationForm from './component/RegistrationForm';
+import LoginForm from './component/Login';
+
 
 function App() {
   return (
     <Routes>
       <Route path="/registration" element={<RegistrationForm />} />
+      <Route path="/login" element={<LoginForm />} />
+
+
     </Routes>
   );
 }
