ad8c6ba88c97c3861629811d45c944dfa71dd3dd
shxrox <sharondeva8@gmail.com>
2025-05-07 15:13:42 +0530

    Lab Navbar

---
+++ BloodLine-Frontend/src/component/dashboard/lab/LabNavbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NurseNavbar = () => {
+  return (
+    <nav style={styles.navbar}>
+      <div>
+        <Link to="/dashboard/lab" style={styles.link}>Home</Link>
+        <Link to="/dashboard/lab/lab-details" style={styles.link}>lab details</Link>
+        <Link to="/dashboard/lab/lab-cheking" style={styles.link}>lab check up details</Link>
+        <Link to="/login" style={styles.link}>Logout</Link>
+      </div>
+    </nav>
+  );
+};
+
+const styles = {
+  navbar: {
+    backgroundColor: "green",
+    padding: "10px",
+    display: "flex",
+    justifyContent: "center",
+  },
+  link: {
+    margin: "0 15px",
+    color: "white",
+    textDecoration: "none",
+    fontWeight: "bold",
+  }
+};
+
+export default NurseNavbar;
