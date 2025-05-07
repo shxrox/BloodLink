f4ab8ebadd75438bc343cb97801b67e91eaa74eb
shxrox <sharondeva8@gmail.com>
2025-05-07 15:12:45 +0530

    Rounding updated

--- BloodLine-Frontend/src/component/dashboard/lab/lab.jsx
+++ BloodLine-Frontend/src/component/dashboard/lab/lab.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "./LabNavbar";
 
 const Nurse = () => {
   return (
     <div>
+      <Navbar />
       <h1>Welcome Lab Tech</h1>
       <p>You have successfully logged in.</p>
     </div>
