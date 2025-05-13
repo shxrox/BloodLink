7751cde44096b046db544268081b77764145c7d6
shxrox <sharondeva8@gmail.com>
2025-05-13 17:12:00 +0530

    doctor description

---
+++ BloodLine-Frontend/src/services/doctorDescriptionService.js
@@ -0,0 +1,28 @@
+// src/services/doctorDescriptionService.js
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:8080/api/doctor-descriptions';
+
+const DoctorDescriptionService = {
+  addDescription: (patientId, description) => {
+    return axios.post(`${BASE_URL}/${patientId}`, description);
+  },
+
+  getByPatient: (patientId) => {
+    return axios.get(`${BASE_URL}/patient/${patientId}`);
+  },
+
+  getAll: () => {
+    return axios.get(BASE_URL);
+  },
+
+  updateDescription: (id, updatedDescription) => {
+    return axios.put(`${BASE_URL}/${id}`, updatedDescription);
+  },
+
+  deleteDescription: (id) => {
+    return axios.delete(`${BASE_URL}/${id}`);
+  }
+};
+
+export default DoctorDescriptionService;
