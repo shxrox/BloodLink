271af009d7a9bcdbea53eb1d375ddf45278ce5c3
shxrox <sharondeva8@gmail.com>
2026-03-28 15:47:44 +0530

    onvert all import paths and component filenames to lowercase for Netlify build compatibility

--- BloodLine-Frontend/src/App.jsx
+++ BloodLine-Frontend/src/App.jsx
@@ -1,27 +1,26 @@
 import { Routes, Route } from 'react-router-dom';
-import RegistrationForm from './component/RegistrationForm';
-import LoginForm from './component/Login';
+import RegistrationForm from './component/registrationform';
+import LoginForm from './component/login';
 import Doctor from './component/dashboard/doctor/doctor';
 import Lab from './component/dashboard/lab/lab';
-import Patientregister from "./component/dashboard/nurse/Patientregister";
-import Patientdetails from "./component/dashboard/doctor/Patientdetails";
-import Patientqueue from "./component/dashboard/nurse/PatientQueue";
-import Home from './component/Home';
-import Patientcheking from "./component/dashboard/doctor/LabDetailed";
-import AbbLab from "./component/dashboard/nurse/AddLabReport";
-import LabInfo from './component/dashboard/lab/LabSubmitInfo';
-import Labdetails from './component/dashboard/lab/Labdetails';
-import NurseLabDatils from './component/dashboard/nurse/LabDetailed';
-import PetientDescription from './component/dashboard/doctor/DoctorDescription';
-import Medicine from './component/dashboard/doctor/MedicineManagement';
-import ManagementDoc from './component/dashboard/doctor/AccountManagement';
-import ManagementLab from './component/dashboard/lab/AccountManagement';
-import ManagementNurse from './component/dashboard/nurse/AccountManagement';
-import AboutD from './component/dashboard/doctor/About';
-import AboutL from './component/dashboard/lab/About';
-import AboutN from './component/dashboard/nurse/About';
-// Fixed the line below to match the lowercase folder structure on GitHub
-import Nurse from './component/dashboard/nurse/Nurse'; 
+import PatientRegister from "./component/dashboard/nurse/patientregister";
+import PatientDetails from "./component/dashboard/doctor/patientdetails";
+import PatientQueue from "./component/dashboard/nurse/patientqueue";
+import Home from './component/home';
+import PatientChecking from "./component/dashboard/doctor/labdetailed";
+import AddLabReport from "./component/dashboard/nurse/addlabreport";
+import LabInfo from './component/dashboard/lab/labsubmitinfo';
+import LabDetails from './component/dashboard/lab/labdetails';
+import NurseLabDetails from './component/dashboard/nurse/labdetailed';
+import PatientDescription from './component/dashboard/doctor/doctordescription';
+import MedicineManagement from './component/dashboard/doctor/medicinemanagement';
+import ManagementDoc from './component/dashboard/doctor/accountmanagement';
+import ManagementLab from './component/dashboard/lab/accountmanagement';
+import ManagementNurse from './component/dashboard/nurse/accountmanagement';
+import AboutDoctor from './component/dashboard/doctor/about';
+import AboutLab from './component/dashboard/lab/about';
+import AboutNurse from './component/dashboard/nurse/about';
+import Nurse from './component/dashboard/nurse/nurse';
 
 function App() {
   return (
@@ -30,27 +29,30 @@ function App() {
       <Route path="/login" element={<LoginForm />} />
       <Route path="/registration" element={<RegistrationForm />} />
 
+      {/* Nurse Routes */}
       <Route path="/dashboard/nurse" element={<Nurse />} />
-      <Route path="/dashboard/nurse/patient-register" element={<Patientregister />} />
-      <Route path="/dashboard/nurse/patient-labreport" element={<AbbLab />} />
-      <Route path="/dashboard/nurse/patient-queue" element={<Patientqueue />} />
-      <Route path="/dashboard/nurse/lab-cheking" element={<NurseLabDatils />} />
+      <Route path="/dashboard/nurse/patient-register" element={<PatientRegister />} />
+      <Route path="/dashboard/nurse/patient-labreport" element={<AddLabReport />} />
+      <Route path="/dashboard/nurse/patient-queue" element={<PatientQueue />} />
+      <Route path="/dashboard/nurse/lab-cheking" element={<NurseLabDetails />} />
       <Route path="/dashboard/nurse/staff" element={<ManagementNurse />} />
-      <Route path="/dashboard/nurse/about" element={<AboutN />} />
+      <Route path="/dashboard/nurse/about" element={<AboutNurse />} />
 
+      {/* Doctor Routes */}
       <Route path="/dashboard/doctor" element={<Doctor />} />
-      <Route path="/dashboard/doctor/patient-details" element={<Patientdetails />} />
-      <Route path="/dashboard/doctor/petient-cheking" element={<Patientcheking />} />
-      <Route path="/dashboard/doctor/petient-description" element={<PetientDescription />} />
-      <Route path="/dashboard/doctor/petient-medicine" element={<Medicine />} />
+      <Route path="/dashboard/doctor/patient-details" element={<PatientDetails />} />
+      <Route path="/dashboard/doctor/petient-cheking" element={<PatientChecking />} />
+      <Route path="/dashboard/doctor/petient-description" element={<PatientDescription />} />
+      <Route path="/dashboard/doctor/petient-medicine" element={<MedicineManagement />} />
       <Route path="/dashboard/doctor/staff" element={<ManagementDoc />} />
-      <Route path="/dashboard/doctor/about" element={<AboutD />} />
+      <Route path="/dashboard/doctor/about" element={<AboutDoctor />} />
 
+      {/* Lab Routes */}
       <Route path="/dashboard/lab" element={<Lab />} />
       <Route path="/dashboard/lab/lab-details" element={<LabInfo />} />
-      <Route path="/dashboard/lab/lab-cheking" element={<Labdetails />} />
+      <Route path="/dashboard/lab/lab-cheking" element={<LabDetails />} />
       <Route path="/dashboard/lab/staff" element={<ManagementLab />} />
-      <Route path="/dashboard/lab/about" element={<AboutL />} />
+      <Route path="/dashboard/lab/about" element={<AboutLab />} />
     </Routes>
   );
 }
