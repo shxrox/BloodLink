4d43ffc2907b9e1025054e75839c496ba709e32a
shxrox <sharondeva8@gmail.com>
2026-04-01 11:21:58 +0530

    updating the file

--- src/main/java/com/example/bloodlinkbackend/Repository/LabReportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bloodlinkbackend.Repository;
-
-import com.example.bloodlinkbackend.Model.LabReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface LabReportRepository extends JpaRepository<LabReport, Long> {
-    List<LabReport> findByPatientFullName(String fullName);
-}
